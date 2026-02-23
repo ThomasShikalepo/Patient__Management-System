44878a08d6f8a51d6ec6cd945c56c56d9b0528a7
Deon <thomasshikalepo@gmail.com>
2026-02-23 12:38:33 +0200

    init commit

---
+++ Patient-service/src/main/java/com/pm/patientservice/model/Patient.java
@@ -0,0 +1,92 @@
+package com.pm.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+public class Patient {
+   @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID patientId;
+
+   @NotNull
+    private String firstname;
+
+   @NotNull
+    private String lastname;
+
+   @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+   @NotNull
+    private String address;
+
+   @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private String registeredDate;
+
+    public UUID getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(UUID patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+
+}
