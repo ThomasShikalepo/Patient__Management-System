28c3b2b1f6f101289a4ad50344ec99ca9c23b356
Deon <thomasshikalepo@gmail.com>
2026-03-09 17:49:16 +0200

    add Kafka producer to publish PatientEvent messages

---
+++ Patient-service/src/main/java/com/pm/patientservice/Config/KafkaConfig.java
@@ -0,0 +1,50 @@
+package com.pm.patientservice.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+@RequiredArgsConstructor
+public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    private Map<String, Object> senderProps() {
+
+        Map<String, Object> props = new HashMap<>();
+
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 10);
+
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class);
+
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, byte[]> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(senderProps());
+    }
+
+    @Bean
+    public KafkaTemplate<String, byte[]> kafkaTemplate(
+            ProducerFactory<String, byte[]> producerFactory) {
+
+        return new KafkaTemplate<>(producerFactory);
+    }
+}
